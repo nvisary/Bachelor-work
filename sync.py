ef1af486ee6087cb4e1e17508e5678a836b15b48
nvisary <nvisary@gmail.com>
2018-11-26 19:25:15 +0300

    Add comments to sync

--- sync.py
+++ sync.py
@@ -2,11 +2,11 @@ import pygame
 import time
 from pydub import AudioSegment
 
-
 import text_utils.book
 
 CUTTED_MP3_PATH = "res/cutted.mp3"
 PLAY_MUSIC = True
+PLAY_MUSIC_TIME = 10  # play first seconds
 
 
 class Sync:
@@ -16,15 +16,19 @@ class Sync:
         self.book_path = book_path
 
     def sync_from(self, second):
+        # load audio, open book, open preprocessor file
         mp3_audio = AudioSegment.from_mp3(self.mp3_path)
         book = text_utils.book.BookWorker(self.book_path)
         preprocessor_file = open(self.preprocessor_path, "r")
 
+        # get book text (for fb2 return text between body tag)
         book_text = book.get_book_text()
 
+        # cut audio by sec and save it
         mp3_audio = mp3_audio[second * 1000:]
         mp3_audio.export(CUTTED_MP3_PATH, format="mp3")
 
+        # use pygame to play cutted audio
         if PLAY_MUSIC:
             pygame.init()
 
@@ -33,21 +37,28 @@ class Sync:
 
             pygame.mixer.music.load(CUTTED_MP3_PATH)
             pygame.mixer.music.play()
-            time.sleep(10)
+            time.sleep(PLAY_MUSIC_TIME)
             pygame.mixer.music.stop()
 
+        # parse preprocessor file
         preprocessor_parameters = []
         for line in preprocessor_file:
             preprocessor_parameters.append(int(line))
 
+        # get min block size from preprocessor
         block_size = preprocessor_parameters[0]
+
+        # count blocks * block_size = how long audio
         count_seconds = (len(preprocessor_parameters) - 1) * block_size
         if second < count_seconds:
             count_blocks = second / block_size
-            print(count_blocks)
+
+            # count words in blocks (how many words in count_blocks)
             count_words = 0
             for i in range(int(round(count_blocks))):
                 count_words += preprocessor_parameters[i + 1]
+
+            # print text from count_words
             book_text = book_text.split()
             book_text = book_text[count_words:]
             print(" ".join(book_text))
